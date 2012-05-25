3a91453eb0b8861d23b2712f4a71b7670b16d2e6
Jan Xie <jan.h.xie@gmail.com>
2012-05-25 18:06:13 +0800

    add README

---
+++ README.markdown
@@ -0,0 +1,19 @@
+# Xmonad Applet for KDE4 (Plasma)
+
+This is a fork of Igor Almeida's work at: git://gitorious.org/xmonad-log-plasmoid/xmonad-log-plasmoid.git
+
+With this applet you can see your xmonad workspaces, layout name and window title in plasma panel.
+
+This applet pipe log through dbus like https://github.com/alexkay/xmonad-log-applet
+
+# Main Improvements
+
+  * BETTER DOCUMENT
+  * Auto expanding in panel
+
+# Install
+
+  * Edit your xmonad.hs, see xmonad.hs for example
+  * In repo directory, run `make` and `make install`
+  * Applet is already installed, choose xmonad-log-plasmoid applet and drag to panel
+
---
+++ xmonad.hs
@@ -0,0 +1,40 @@
+import XMonad
+import XMonad.Config.Kde
+import XMonad.Hooks.DynamicLog
+
+import Control.OldException
+
+import DBus
+import DBus.Connection as DC
+import DBus.Message
+
+main :: IO ()
+main =  withConnection Session $ \dbus -> do
+    getWellKnownName dbus
+    xmonad $ kdeConfig
+         { logHook = dynamicLogWithPP (prettyPrinter dbus)
+         }
+
+prettyPrinter :: DC.Connection -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput   = dbusOutput dbus
+    , ppCurrent  = wrap "[" "]"
+    , ppSep      = " | "
+    }
+
+getWellKnownName :: DC.Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\(DBus.Error _ _) -> getWellKnownName dbus)
+  where
+    tryGetName = do
+        namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+        addArgs namereq [String "org.xmonad.Log", Word32 5]
+        sendWithReplyAndBlock dbus namereq 0
+        return ()
+
+dbusOutput :: DC.Connection -> String -> IO ()
+dbusOutput dbus str = do
+    msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" "Update"
+    addArgs msg [String ("<b>" ++ str ++ "</b>")]
+    -- If the send fails, ignore it.
+    send dbus msg 0 `catchDyn` (\(DBus.Error _ _) -> return 0)
+    return ()
